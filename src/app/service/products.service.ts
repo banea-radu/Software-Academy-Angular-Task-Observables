4443a599aabe195a44dbbf435ebbd1fb9d9e5def
banea-radu <100232231+banea-radu@users.noreply.github.com>
2022-12-21 09:24:04 +0200

    1 file modified

--- src/app/service/products.service.ts
+++ src/app/service/products.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-// import { BehaviorSubject } from 'rxjs';
-// import { ProductModel } from '../model/product.model';
 
 @Injectable({
   providedIn: 'root'
@@ -27,38 +25,7 @@ export class ProductsService {
     }
   ]
 
-  // private products = new BehaviorSubject<any>(
-  //   [
-  //     {
-  //       "id":1,
-  //       "title":"Product 1",
-  //       "price":20.00,
-  //       "description":"Product 1 decription."
-  //     },
-  //     {
-  //       "id":2,
-  //       "title":"Product 2",
-  //       "price":10.0,
-  //       "description":"Product 2 description."
-  //     },
-  //     {
-  //       "id":3,
-  //       "title":"Product 3",
-  //       "price":5.00,
-  //       "description":"Product 3 description."
-  //     }
-  //   ]
-  // );
-
-  // constructor() { }
-
   getProducts(){
     return this.products;
   }
-
-  // getProducts(){
-  //   // this.subject = this.productsList;
-  //   // console.log(this.products);
-  //   return this.products;
-  // }
 }
